68aadaf8a32083830c4e6c6ad65e2be867da8821
Hisma <richard.meyer596@gmail.com>
2025-04-23 18:27:20 -0400

    convert page to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,40 +5,39 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import embeds from "astro-embed/integration";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
   output: "server",
   adapter: netlify(),
   site: "https://productiv-ai.netlify.app/",
-  integrations: [
-    embeds(), // Add this line
-    starlight({
-      title: "ProductivAI Docs",
-      customCss: ["./src/styles/global.css"],
-      social: [
-        {
-          icon: "github",
-          label: "Github",
-          href: "https://github.com/Hisma/productiv-ai-site",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Start Learning",
-          items: [
-            {
-              label: "Deploy Linux & Docker/Portainer",
-              slug: "start/wsl2-portainer",
-            },
-            {
-              label: "Install OpenWebUI and Ollama",
-              slug: "start/ollama-open-webui",
-            },
-          ],
-        },
-      ],
-    }),
-    icon()
-  ],
+  integrations: [// Add this line
+  embeds(), starlight({
+    title: "ProductivAI Docs",
+    customCss: ["./src/styles/global.css"],
+    social: [
+      {
+        icon: "github",
+        label: "Github",
+        href: "https://github.com/Hisma/productiv-ai-site",
+      },
+    ],
+    sidebar: [
+      {
+        label: "Start Learning",
+        items: [
+          {
+            label: "Deploy Linux & Docker/Portainer",
+            slug: "start/wsl2-portainer",
+          },
+          {
+            label: "Install OpenWebUI and Ollama",
+            slug: "start/ollama-open-webui",
+          },
+        ],
+      },
+    ],
+  }), icon(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
